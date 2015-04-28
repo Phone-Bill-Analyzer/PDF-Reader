8e6e4b50a3139870740bc4bcf8eff7b1f0a2b48a
Varun Verma <varunverma@varunverma.org>
2015-04-28 17:44:23 +0800

    Need to solve 1 problem

--- src/com/ayansh/pdfreader/billparser/TataDocomoPostPaidMobileBill.java
+++ src/com/ayansh/pdfreader/billparser/TataDocomoPostPaidMobileBill.java
@@ -3,6 +3,7 @@
  */
 package com.ayansh.pdfreader.billparser;
 
+import java.io.Console;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -23,6 +24,8 @@ public class TataDocomoPostPaidMobileBill extends PhoneBill {
 		billType = TataDocomoPostPaidMobileBill.BillType;
 	}
 	
+	private String current_date = "";
+	private String tag = "";
 	
 	@Override
 	public void parseBillText() {
@@ -82,9 +85,7 @@ public class TataDocomoPostPaidMobileBill extends PhoneBill {
 		// Itemized Bill Details
 		pages = fileText.split("Your Itemized Details");
 		page = pages[1];
-		pages = page.split("Page ");
-		
-		String current_date = "";
+		pages = page.split("Page |2G|3G|NR|SMS|Messaging");
 		
 		for(int i=0; i<pages.length; i++){
 			
@@ -96,92 +97,150 @@ public class TataDocomoPostPaidMobileBill extends PhoneBill {
 			for(int j=0; j<lines.length; j++){
 				
 				line = lines[j];
-				words = line.split(" ");
 				
-				if(words.length > 1 && words[0].charAt(2) == '/'){
-					
-					try {
-						
-						Date date = sdf1.parse(words[0]);
-						current_date = sdf2.format(date);
-						
-						words[0] = words[0] + " VV VV VV VV VV";
-						line = "";
-						for(int v=0; v<words.length; v++){
-							if(v == 0){
-								line = words[v];
-							}
-							else{
-								line = line + " " + words[v];
-							}
-						}
-						
-						lines[j] = line;
-						
-						continue;
-						
-					} catch (ParseException e) {
-						continue;
-					}				
-				}
+				get_tag(line);
 				
-				if(words.length >=6 && !current_date.contentEquals("")){
-					
-					CallDetailItem pbi = new CallDetailItem();
-					
-					pbi.setCallDate(current_date);
-					
-					pbi.setCallTime(words[0]);
-					
-					pbi.setPhoneNumber(words[1]);
-					pbi.setDuration(words[3]);
-					
-					pbi.setCost(Float.valueOf(words[5]));
-					
-					callDetails.add(pbi);
+				line = process_line(line, 1);
+				
+				if(!line.contentEquals("")){
+					lines[j] = line;
 				}
 				
 			}
 			
-			// 2nd half of lines
+			// 2nd half of lines.
 			for(int j=0; j<lines.length; j++){
-				
+							
 				line = lines[j];
-				words = line.split(" ");
-				
-				if(words[6].charAt(2) == '/'){
-					
-					try {
-						
-						Date date = sdf1.parse(words[0]);
-						current_date = sdf2.format(date);
-						continue;
-						
-					} catch (ParseException e) {
-						continue;
-					}				
-				}
-				
-				if(words.length >=12 && !current_date.contentEquals("")){
-					
-					CallDetailItem pbi = new CallDetailItem();
-					
-					pbi.setCallDate(current_date);
-					
-					pbi.setCallTime(words[6]);
-					
-					pbi.setPhoneNumber(words[7]);
-					pbi.setDuration(words[9]);
-					
-					pbi.setCost(Float.valueOf(words[11]));
-					
-					callDetails.add(pbi);
-				}
-				
+				get_tag(line);
+				process_line(line, 2);
+							
 			}
+		}
+	}
+
+	private String process_line(String line, int part) {
+		
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yy");
+		SimpleDateFormat sdf2 = new SimpleDateFormat("dd-MMM-yyyy");
+		
+		System.out.println(line);
+		String[] words = line.split(" ");
+		
+		int minLength, beginIndex, maxLength;
+		int callTimeIndex, phoneNumIndex, durationIndex, costIndex;
+		
+		// Part1 => 1, Part 2 => 6
+		minLength = (part - 1)*5 + 1;
+		
+		// Part1 => 0, Part 2 => 6
+		beginIndex = (part - 1)*6;
+		
+		// Part 1 =>6, Part 2 => 12
+		maxLength = part*6;
+		
+		// Part1 => 0, Part 2 => 6
+		callTimeIndex = (part - 1)*6;
+		
+		// Part1 => 1, Part 2 => 7
+		phoneNumIndex = callTimeIndex + 1;
+		
+		// Part1 => 3, Part 2 => 9
+		durationIndex = callTimeIndex + 3;
+		
+		// Part1 => 5, Part 2 => 11
+		costIndex = callTimeIndex + 5;
+		
+		if(words.length > minLength && words[beginIndex].length() >2 && words[beginIndex].charAt(2) == '/'){
 			
+			try {
+				
+				Date date = sdf1.parse(words[beginIndex]);
+				current_date = sdf2.format(date);
+				
+				if(part == 1){
+					
+					words[beginIndex] = words[beginIndex] + " VV VV VV VV VV";
+					line = "";
+					for(int v=0; v<words.length; v++){
+						if(v == 0){
+							line = words[v];
+						}
+						else{
+							line = line + " " + words[v];
+						}
+					}
+					
+					return line;
+				}
+				
+			} catch (ParseException e) {
+				return "";
+			}				
 		}
 		
+		if(words.length >=maxLength && words[beginIndex].length() >2 && words[beginIndex].charAt(2) == ':' && !current_date.contentEquals("")){
+			
+			CallDetailItem pbi = new CallDetailItem();
+			
+			pbi.setCallDate(current_date);
+			
+			pbi.setCallTime(words[callTimeIndex]);
+			
+			pbi.setPhoneNumber(words[phoneNumIndex]);
+			
+			if(tag.contentEquals("SMS")){
+				pbi.setDuration("");
+			}
+			else{
+				pbi.setDuration(words[durationIndex]);
+			}
+			
+			pbi.setCost(Float.valueOf(words[costIndex]));
+			
+			// Special Attributes
+			if(tag.contentEquals("STD")){
+				pbi.setStdCall("X");
+			}
+			
+			if(tag.contentEquals("2G")){
+				pbi.setPhoneNumber("data");
+				pbi.setComments("2G");
+			}
+			
+			if(tag.contentEquals("3G")){
+				pbi.setPhoneNumber("data");
+				pbi.setComments("2G");
+			}
+			
+			if(tag.contentEquals("Roaming")){
+				pbi.setRoamingCall("X");
+			}
+			
+			if(tag.contentEquals("SMS")){
+				pbi.setSmsCall("X");
+			}
+			
+			callDetails.add(pbi);
+		}
 		
+		return "";
+	}
+		
+	private void get_tag(String line) {
+
+		if (line.contains("Local")) {
+			tag = "Local";
+		} else if (line.contains("STD")) {
+			tag = "STD";
+		} else if (line.contains("Data Charges 2G")) {
+			tag = "2G";
+		} else if (line.contains("Data Charges 3G")) {
+			tag = "3G";
+		} else if (line.contains("NR")) {
+			tag = "Roaming";
+		} else if (line.contains("SMS") || line.contains("Messaging")) {
+			tag = "SMS";
+		}
 	}
 }
